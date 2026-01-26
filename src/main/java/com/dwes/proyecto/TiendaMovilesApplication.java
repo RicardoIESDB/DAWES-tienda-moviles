8e50d3f829e5e1203824d58219747cadbc5620b7
RicardoIESDB <rl.aucapina@iesdoctorbalmis.com>
2026-01-26 11:59:38 +0100

    creacion de proyecto

---
+++ src/main/java/com/dwes/proyecto/TiendaMovilesApplication.java
@@ -0,0 +1,13 @@
+package com.dwes.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaMovilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaMovilesApplication.class, args);
+    }
+
+}
