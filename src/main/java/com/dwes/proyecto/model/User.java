8e86ed0ef8d84a990f99fc9bac7151cd3fc05140
RicardoIESDB <rl.aucapina@iesdoctorbalmis.com>
2026-02-04 11:32:51 +0100

    User.java corregido

--- src/main/java/com/dwes/proyecto/model/User.java
+++ src/main/java/com/dwes/proyecto/model/User.java
@@ -15,12 +15,24 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = username)
+    @Column(nullable = false)
     private String username;
 
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
     private String password;
 
-    private Enum role;
+    @Column(nullable = false)
+    private Boolean administrador;
 
-    private Boolean enable;
+    @Column(nullable = false)
+    private Boolean usuario;
+
+    @Column(nullable = false)
+    private Boolean invitado;
+
+    @Column(nullable = false)
+    private Boolean activado;
 }
